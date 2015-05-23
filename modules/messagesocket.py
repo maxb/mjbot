4b59fe17f17eaed0d21f21ce3ccbc8d00c5b3fbe
Max Bowsher <_@maxb.eu>
2015-05-23 18:29:03 +0100

    More frustrations in socket handling.

--- modules/messagesocket.py
+++ modules/messagesocket.py
@@ -1,25 +1,40 @@
 from threading import Thread
 import socket
 
+"""
+It appears to be strangely challenging to close a listening socket and re-open
+it within a Python process, without running into 'address in use' errors. Even
+after experimenting with a variety of non-blocking options, and attempting to
+manually make a connection to the listening socket to knock another thread out
+of a blocking accept call, nothing seemed to work. Thus, in the interests of
+achieving a working, if somewhat weird, solution, the server socket is now
+cached on the bot object beyond the lifetime of the plugin module. Even with
+that measure, the first connection after a module reload attempt will still go
+to the old code and thread (after which it will then exit and leave the new
+code and thread to carry on).
+"""
+
 class SocketServer(Thread):
     def __init__(self, bot):
         Thread.__init__(self)
         self.daemon = True
         self.bot = bot
         self.stop = False
-        self.ssock = None
+        self.addr = ('127.0.0.1', int(self.bot.config.messagesocket.port))
+
+        self.ssock = getattr(bot, 'messagesocket_ssock', None)
+        if self.ssock is None:
+            self.ssock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.ssock.bind(self.addr)
+            self.ssock.listen(5)
+            bot.messagesocket_ssock = self.ssock
+
         self.start()
 
     def request_stop(self):
-        print("{} closing socket".format(self))
         self.stop = True
-        self.ssock.close()
 
     def run(self):
-        print("{} opening socket".format(self))
-        self.ssock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.ssock.bind(('127.0.0.1', int(self.bot.config.messagesocket.port)))
-        self.ssock.listen(5)
         while not self.stop:
             sock, addr = self.ssock.accept()
             ConnHandler(self.bot, sock).start()
